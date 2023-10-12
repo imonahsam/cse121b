fc256be30992ec7f598f26d7cd1a32d91cec5d1b
imonahsm <samimonah@gmail.com>
2023-10-12 22:04:05 +0100

    Temple display assignment

---
+++ fetch.js
@@ -0,0 +1,48 @@
+// fetch,js
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
+let results = null;
+
+async function getPokemon(url, doThis) {
+
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doThis(data);
+  }
+}
+
+// async function getPokemonList(url) {
+//     const response = await fetch(url);
+//     if (response.ok) {
+//         const data = await response.json();
+//         doStuffList(data);
+
+
+//     }
+// }
+function doStuff(data) {
+    results = data;
+    const displayElement = document.querySelectorAll("#output");
+    const html = `<h2>"${data.name}<h2><img src="${data.sprites.front_default}" alt="Image of ${data.name}">`;
+    displayElement.innerHTML = html;
+    console.log("first: ", results);
+}
+
+function doStuffList(data) {
+    console.log(data);
+    const pokeListElement = document.querySelector("#outputList");
+    const pokeList = data.results;
+    pokeList.forEach((currentItem) =>{
+        const html = `<li>${currentItem.name}</li>`;
+        pokeListElement.innerHTML += html;
+    });
+}
+
+getPokemon(url);
+console.log("second: ", results);
+
+getPokemonList(urlList, doStuffList);
---
+++ scripts/w05-task.js
@@ -0,0 +1,65 @@
+/* W05: Programming Tasks */
+const templesElement = document.querySelector("#temples");
+
+/* Declare and initialize global variables */
+
+let templesList = [];
+/* async displayTemples Function */
+
+const displayTemples = (templesList) => {
+   templesList.forEach((temples) => {
+    let article = document.createElement("article");
+    let templeNameTag = document.createElement("h3");
+    templeNameTag.innerHTML = temples.templeName;
+
+    const templeImage = document.createElement("img");
+    templeImage.src = temples.imageUrl;
+    templeImage.alt = temples.location;
+    article.appendChild(templeNameTag);
+    article.appendChild(templeImage);
+    templesElement.appendChild(article); 
+   });
+};
+
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    if (response.ok) {
+        // the API will send us JSON...but we have to convert the response before we can use it
+        // .json() also returns a promise...so we await it as well.
+        templesList = await response.json();
+        displayTemples(templesList);
+
+    }
+};
+/* async getTemples Function using fetch()*/
+/* reset Function */
+let reset = (templesList) => {
+    templesElement.innerHTML = "";
+};
+const sortBy = () => {
+    reset();
+    const filter = document.getElementById("sortBy").value;
+    switch (filter) {
+        case "utah":
+            displayTemples(templesList.filter(temple => temple.location.includes("Utah")));          
+            break;
+        case "nonutah":
+            displayTemples(templesList.filter(temple => !temple.location.includes("Utah")));
+            break;
+        case "older":
+            displayTemples(templesList.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case "all":
+        default:
+            displayTemples(templesList);
+            
+    }
+
+};
+
+
+document.getElementById("sortBy").addEventListener("change", () => {sortBy(templesList)});
+getTemples();
+// console.log(templeList);
+/* Event Listener */
+/* sortBy Function */
